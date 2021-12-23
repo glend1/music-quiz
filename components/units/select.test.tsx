00e4c8b04dc89a376866efeaf6faee57993ad5f4
Glen <psycotik666@gmail.com>
2021-12-23 14:07:53 +0000

    added tests for the select component

---
+++ components/units/select.test.tsx
@@ -0,0 +1,24 @@
+import { render, waitFor, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ChangeEvent } from 'react'
+import { Select } from './select'
+
+describe("Select", () => {
+    it("Should have the correct data", async () => {
+        var container: HTMLElement
+        await waitFor(() => {
+            ({container} = render(<Select label={'label'} array={["data1", "data2", "data3"]} id={'test'} cb={function (event: ChangeEvent<HTMLSelectElement>): void {} } />))
+        }).then(() => {
+            expect(container.querySelector('#test')).toBeTruthy()
+            expect(container.querySelector('label')).toHaveTextContent("label")
+            expect(container.querySelectorAll('option')).toHaveLength(3)
+        })
+    })
+    it("Should trigger the callback when the select has recieved a change event", () => {
+        const callback = jest.fn();
+        render(<Select label={'label'} array={["data1", "data2", "data3"]} id={'test'} cb={callback} />)
+        const select = screen.getByRole('combobox')
+        userEvent.selectOptions(select, ['data2'])
+        expect(callback).toHaveBeenCalledTimes(1);
+    })
+})
