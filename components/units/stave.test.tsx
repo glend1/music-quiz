3f4acefb330359e4cc9a1704a8dce02b87efae38
Glen <psycotik666@gmail.com>
2021-12-23 13:13:01 +0000

    added a test for test the passed in id for the stave component

--- components/units/stave.test.tsx
+++ components/units/stave.test.tsx
@@ -11,6 +11,14 @@ describe("Stave Component", () => {
             expect(container.querySelector('[data-name="note"]')).toBeTruthy()
         })
     })
+    it("Should render div with the correct ID", async () => {
+        var container: HTMLElement
+        await waitFor(() => {
+            ({container} = render(<Stave notation={RandomNote()!.abc} id="question"/>))
+        }).then(() => {
+            expect(container.querySelector('#question')).toBeTruthy()
+        })
+    })
     it("Shouldn't render an note", async () => {
         var container: HTMLElement
         await waitFor(() => {
