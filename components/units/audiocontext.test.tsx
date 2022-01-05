7eaa6e025bf6aca8b8dbcbe79e029fa15eb5d0fb
Glen <psycotik666@gmail.com>
2022-01-05 14:59:23 +0000

    added tests to the audiocontext component

---
+++ components/units/audiocontext.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux';
+import { AudioContext } from './audiocontext'
+import { AudioContext as ACMock } from 'standardized-audio-context-mock';
+import { Store } from "../../util/store"
+import userEvent from '@testing-library/user-event';
+
+describe("audioContext", () => {
+    it("Should give the user a button to start the AudioContext", () => {
+        render(<Provider store={Store}><AudioContext/></Provider>)
+        expect(screen.getByRole("button")).not.toBeDisabled()
+    })
+    it("Should disable the button when there is an AudioContext", () => {
+        const ac = global.AudioContext
+        global.AudioContext = ACMock as any;
+        render(<Provider store={Store}><AudioContext/></Provider>)
+        userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("button")).toBeDisabled()
+        global.AudioContext = ac
+    })
+})
