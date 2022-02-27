69444141ed628beb9df4d4115340889a72c0b9c0
Glen <psycotik666@gmail.com>
2022-02-27 16:14:28 +0000

    mocking canvas is no the way to go for doing chart.js tests

--- components/units/audiograph.test.tsx
+++ components/units/audiograph.test.tsx
@@ -1,19 +1,34 @@
-import { render, waitFor, screen } from "@testing-library/react"
+import { render, RenderResult, waitFor } from "@testing-library/react"
 import { AudioGraph } from "./audiograph"
+import { queries } from "@testing-library/dom/types/"
 
-describe("audiograph", () => {
-    //TODO more tests need to be done
-    it("Should render a canvas", async () => {
-        // class ResizeObserver {
-        //     observe() {}
-        //     disconnect() {}
-        // }
-        // window.ResizeObserver = ResizeObserver as any;
-        var container: HTMLElement
-        await waitFor(() => {
-            ({container} = render(<AudioGraph freq={500}/>))
-        }).then(() => {
-            expect(container.querySelector('canvas')).toBeTruthy()
-        })
-    })
-})
+//TODO this can't be unit tested
+// describe("audiograph", () => {
+//     beforeAll(() => {
+//         class ResizeObserver {
+//             observe() {}
+//             disconnect() {}
+//         }
+//         window.ResizeObserver = ResizeObserver as any;
+//     })
+//     it("Should render a canvas", async () => {
+//         let ag: RenderResult<typeof queries, HTMLElement>
+//         await waitFor(() => {
+//             ag = render(<AudioGraph freq={500}/>)
+//         }).then(() => {
+//             expect(ag.container.querySelector('canvas')?.getContext("2d")?.__getDrawCalls()).toMatchSnapshot()
+//         })
+//     })
+//     it("Should fill up the graph with data", async () => {
+//         let ag: RenderResult<typeof queries, HTMLElement>
+//         await waitFor(() => {
+//             ag = render(<AudioGraph freq={1}/>)
+//         }).then(() => {
+//             for (let i = 2; i <= 40; i++) {
+//                 ag.rerender(<AudioGraph freq={i}/>)
+//             }
+//         }).then(() => {
+//             expect(ag.container.querySelector('canvas')?.getContext("2d")?.__getDrawCalls()).toMatchSnapshot()
+//         })
+//     })
+// })
