310e20396f5eced075c65fdec98cd4a38c97201c
Glen <psycotik666@gmail.com>
2022-05-21 13:01:17 +0100

    added question playback

--- components/units/osccontrols.tsx
+++ components/units/osccontrols.tsx
@@ -1,16 +1,25 @@
 import { useSelector } from "react-redux";
 import { Select } from "./select";
 import { useArray, useBoolean, useFormState } from "../../util/customHooks";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { State } from "../../util/store";
 import { IStdNote, midiToFrequency } from "../../util/notes";
 import { Slider } from "./slider";
 
-//TODO add a play button
-
 type IOsc = {question: IStdNote[], setAudioEvent: Dispatch<SetStateAction<((type: string, data: IStdNote) => void) | undefined>>}
 
 type IHeld = { data: IStdNote; oscilator: OscillatorNode };
+
+function setupOscillator(ctx: AudioContext, type: string, vol: string) {
+  var osc = ctx.createOscillator();
+  osc.start();
+  osc.type = type as OscillatorType;
+  var volume = ctx.createGain();
+  osc.connect(volume);
+  volume.connect(ctx.destination);
+  volume.gain.value = parseInt(vol) / 100;
+  return osc
+}
 
 export function OscControls({ setAudioEvent, question }: IOsc) {
   const audioContext = useSelector((state: State) => state.context);
@@ -18,26 +27,62 @@ export function OscControls({ setAudioEvent, question }: IOsc) {
   const {bool: play, toggle: togglePlay} = useBoolean(false)
   const [volumeState, setVolume] = useFormState("50");
   const [wave, setWave] = useFormState("triangle");
-
+  const [osc, setOsc] = useState<undefined | OscillatorNode>()
+  const [iId, setIId] = useState<undefined | NodeJS.Timer>()
+  function togglePlaying() {
+    if (audioContext) {
+      togglePlay()
+    }
+  };
+  function playNext(node: OscillatorNode, ctx: AudioContext, queue: IStdNote[]) {
+    if (queue[0]) {
+      node.frequency.setValueAtTime(midiToFrequency(queue[0].midi), ctx.currentTime)
+      queue.shift()
+    }
+  }
+  function clearAndStop(oscilator: OscillatorNode, id: NodeJS.Timer ) {
+      oscilator.stop();
+      setOsc(undefined)
+      if (id) {
+        clearInterval(id)
+        setIId(undefined)
+    }
+  }
+  function playQuestion(ctx: AudioContext, os: OscillatorNode | undefined, type: string, vol: string, question: IStdNote[]) {
+    if (ctx) {
+      if (os && iId) {
+        clearAndStop(os, iId)
+      } else {
+        let queue = [...question]
+        if (queue[0]) {
+          let node = setupOscillator(ctx, type, vol)
+          setOsc(node)
+          playNext(node, ctx, queue)
+          let timer = setInterval(() => {
+            if (queue[0]) {
+              playNext(node, ctx, queue)
+            } else {
+              clearAndStop(node, timer)
+            }
+          }, 500)
+          setIId(timer)
+        }
+      }
+    }
+  }
   useEffect(() => {
     setAudioEvent(() => {
       return (type: string, data: IStdNote) => {
         if (play && audioContext) {
           switch (type) {
             case "start":
-              var osc = audioContext.createOscillator();
-              osc.type = wave as OscillatorType;
-              var volume = audioContext.createGain();
-              osc.connect(volume);
-              volume.connect(audioContext.destination);
-              volume.gain.value = parseInt(volumeState) / 100;
+              let osc = setupOscillator(audioContext, type, volumeState)
               if (data?.midi) {
                 osc.frequency.setValueAtTime(
                   midiToFrequency(data.midi),
                   audioContext.currentTime
                 );
               }
-              osc.start();
               heldPush({ data: data, oscilator: osc });
               break;
             case "stop":
@@ -55,14 +100,6 @@ export function OscControls({ setAudioEvent, question }: IOsc) {
       };
     });
   }, [audioContext, play, volumeState, wave]);
-  function togglePlaying() {
-    if (audioContext) {
-      togglePlay()
-    }
-  };
-  function playQuestion() {
-    console.log(question)
-  };
   return (
     <>
       {audioContext ? (
@@ -73,7 +110,11 @@ export function OscControls({ setAudioEvent, question }: IOsc) {
                 <Slider id="volume" label="Volume" min={1} max={100} value={volumeState} set={setVolume} />
                 <Select id="wave_type" label="Wave Type" array={["triangle", "sine", "square", "sawtooth"]} cb={setWave}/>
               </div><div>
-                <button onClick={playQuestion}>Play Question</button>
+                  { question[0] ? 
+                    <button onClick={() => {playQuestion(audioContext, osc, wave, volumeState, question)}}>{ !osc ? "Play Question" : "Stop Playing"}</button>
+                    :
+                    "No question to play"
+                  }
               </div>
             </>) : (<div></div> )}
           </>
