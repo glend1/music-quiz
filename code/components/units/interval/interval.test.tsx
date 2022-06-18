664294eca80803e6f3f48d2d275f08d70e7825a9
psyco <psycotik666@gmail.com>
2022-06-18 17:31:07 +0100

    added and fixed interval tests

--- code/components/units/interval/interval.test.tsx
+++ code/components/units/interval/interval.test.tsx
@@ -1,29 +1,39 @@
 import { Interval } from './interval'
 import { render, screen, fireEvent } from "@testing-library/react"
-import { useState } from 'react'
-import { IStdNote, StdNote } from '../../../util/extensions/notes/notes'
+import { useQuestionGeneration } from '../../../util/hooks/usequestiongeneration/usequestiongeneration'
+import userEvent from '@testing-library/user-event'
+import { useEffect } from 'react'
 
-// function MockInterval({val}: {val?: IStdNote}) {
-//     const question = useState<IStdNote>(val)
-//     const root = useState<IStdNote>(val)
-//     return <Interval root={root[0]} question={question[0]} setQuestion={question[1]} />
-// }
+function MockInterval({makeRoot = true}) {
+    const questionGeneration = useQuestionGeneration()
+    useEffect(() => {
+        if (makeRoot) {
+            questionGeneration.newRoot(2,26, false)
+        }
+    }, [])
+    return <div><Interval root={questionGeneration.root} newInterval={questionGeneration.newInterval}/><h2>{questionGeneration.interval?.description}</h2></div>
+}
 
-// describe("Interval Component", () => {
-//     afterAll(() => {
-//         jest.spyOn(global.Math, 'random').mockRestore();
-//     })
-//     it("Should prompt to the user that they select an Descending Minor Third", () => {
-//         render(<MockInterval val={StdNote("C4")}/>)
-//         jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-//         fireEvent.change(screen.getByRole("slider"), {target: {value: "6"}})
-//         fireEvent.click(screen.getByRole("button"))
-//         expect(screen.getByRole("heading")).toHaveTextContent("Descending Minor Third")
-//     })
-// })
-// describe("Edge Cases", () => {
-//     it("Should fail to generate an interval", () => {
-//         render(<MockInterval />)
-//         expect(screen.getByText("Please create a Question first")).toBeInTheDocument()
-//     })
-// })
+describe("Interval Component", () => {
+    afterAll(() => {
+        jest.spyOn(global.Math, 'random').mockRestore();
+    })
+    it("Should prompt to the user that they select an Descending Minor Third", () => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+        render(<MockInterval />)
+        fireEvent.change(screen.getByRole("slider"), {target: {value: "6"}})
+        userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("heading")).toHaveTextContent("Descending Minor Third")
+    })
+    it("Should change the direction of the interval", () => {
+        render(<MockInterval />)
+        userEvent.selectOptions(screen.getByRole("combobox"), ['Ascending'])
+        fireEvent.change(screen.getByRole("slider"), {target: {value: "6"}})
+        userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("heading")).toHaveTextContent("Ascending")
+    })
+    it("Should fail to generate an interval", () => {
+        render(<MockInterval makeRoot={false}/>)
+        expect(screen.getByText("Please create a Question first")).toBeInTheDocument()
+    })
+})
