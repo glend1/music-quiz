27b487d6f7900dbd3da6477aa68019bcf6f28d99
psyco <psycotik666@gmail.com>
2022-06-29 11:55:19 +0100

    added tests for scale information

---
+++ code/components/compound/scaleinformation/scaleinformation.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react"
+import { ScaleInformation } from "./scaleinformation"
+
+describe("scaleinformation", () => {
+    it("Should render a default component", () => {
+        render(<ScaleInformation scale={""} root={""} />)
+        expect(screen.getByText("Please provide some Scale infromation")).toBeVisible()
+    })
+    it("Should render a Scale", () => {
+        const element = render(<ScaleInformation scale={"ionian"} root={"C#"} />)
+        expect(element.baseElement).toMatchSnapshot()
+    })
+})
