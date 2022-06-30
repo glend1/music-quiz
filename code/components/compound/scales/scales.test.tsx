6a305f6a8c846ec1c5af1edccc5cc0165ebcd2ec
psyco <psycotik666@gmail.com>
2022-06-30 13:14:07 +0100

    added scale tests

---
+++ code/components/compound/scales/scales.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { Scales } from "./scales"
+
+describe("scales", () => {
+    it("Should render a default component", () => {
+        render(<Scales notes={[]} />)
+        expect(screen.getByText("Please provide some Notes")).toBeVisible()
+    })
+    it("Should render scales", () => {
+        const test = render(<Scales notes={["C", "D", "E"]} />)
+        userEvent.click(test.baseElement.querySelectorAll('path')[0])
+        expect(screen.getAllByRole("img").length).toBe(24)
+    })
+    it("Should return to a default state", () => {
+        const test = render(<Scales notes={["C", "D", "E"]} />)
+        userEvent.click(test.baseElement.querySelectorAll('path')[0])
+        userEvent.click(test.baseElement.querySelectorAll('path')[0])
+        expect(screen.queryAllByRole("img").length).toBe(0)
+    })
+    it("Should show/unshow scales", () => {
+        const test = render(<Scales notes={["C", "D", "E"]} />)
+        userEvent.click(test.baseElement.querySelectorAll('path')[5])
+        userEvent.click(screen.getAllByRole("img")[0])
+        expect(test.baseElement.outerHTML).toMatchSnapshot()
+        userEvent.click(screen.getAllByRole("img")[0])
+        expect(test.baseElement.outerHTML).toMatchSnapshot()
+    })
+
+})
