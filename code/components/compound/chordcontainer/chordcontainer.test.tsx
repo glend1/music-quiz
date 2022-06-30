987bf9b638e5ce65096ae350425cc15fb87151fe
psyco <psycotik666@gmail.com>
2022-06-30 13:27:57 +0100

    added chord container tests

---
+++ code/components/compound/chordcontainer/chordcontainer.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react"
+import { ChordContainer } from "./chordcontainer"
+
+describe("chordcontainer", () => {
+    it("Should render the component", () => {
+        render(<ChordContainer notes={[]} />)
+        expect(screen.getByText("No valid Chord")).toBeVisible()
+    })
+    it("Should render no valid chord", () => {
+        render(<ChordContainer notes={["C", "F"]}/>)
+        expect(screen.getByRole("heading")).toHaveTextContent("Scales")
+    })
+})
