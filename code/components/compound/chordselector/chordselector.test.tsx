b1913fc1801abc32838535bcee597bdb94062956
psyco <psycotik666@gmail.com>
2022-07-02 12:53:40 +0100

    added chord selector tests

---
+++ code/components/compound/chordselector/chordselector.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
+import { TChords } from "../../units/dictionaryutils/dictionaryutils";
+import { ChordSelector } from "./chordselector"
+
+function MockSelector() {
+    const [state, setState] = useState<TChords>({})
+    return <div><h3>{Object.keys(state).join()}</h3><div><ChordSelector chords={setState} /></div></div>
+}
+
+describe("chordselector", () => {
+    it("Should render a default component", () => {
+        const callback = jest.fn();
+        render(<ChordSelector chords={callback} />)
+        expect(screen.getByRole("heading")).toHaveTextContent("Chord")
+    })
+    it("Should select a chord", () => {
+        const selector  = render(<MockSelector />)
+        userEvent.click(selector.baseElement.querySelectorAll('path')[0])
+        userEvent.click(selector.baseElement.querySelectorAll('path')[6])
+        expect(screen.getByText("C5")).toBeVisible()
+    })
+    it("Should delete itself", () => {
+        const selector  = render(<MockSelector />)
+        userEvent.click(selector.baseElement.querySelectorAll('path')[0])
+        userEvent.click(selector.baseElement.querySelectorAll('path')[6])
+        expect(screen.getAllByRole("heading")[0]).not.toHaveTextContent("Chord")
+        userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("heading")).toBeEmptyDOMElement()
+    })
+    it("Should select a Minor Chord, show information and remove it", () => {
+        const selector  = render(<MockSelector />)
+        userEvent.click(selector.baseElement.querySelectorAll('path')[0])
+        userEvent.click(selector.baseElement.querySelectorAll('path')[6])
+        userEvent.click(selector.baseElement.querySelectorAll('path')[4])
+        userEvent.click(screen.getByRole("img"))
+        expect(screen.getAllByText("Cm")).toHaveLength(2)
+        userEvent.click(selector.baseElement.querySelectorAll('path')[0])
+        expect(screen.queryByText("Cm")).toBeNull()
+    })
+})
--- code/components/compound/chordselector/chordselector.tsx
+++ code/components/compound/chordselector/chordselector.tsx
@@ -3,32 +3,53 @@ import { v4 as uuid } from 'uuid';
 import { Piano } from "../../units/piano/piano";
 import kStyles from '../../units/piano/piano.module.css'
 import dStyles from '../../units/dictionaryutils/dictionaryutils.module.css'
-import { TChordMethod, TNotes } from "../../units/dictionaryutils/dictionaryutils";
+import { TChordMethod } from "../../units/dictionaryutils/dictionaryutils";
 import { DeleteParent } from '../../units/dictionaryutils/dictionaryutils'
 import { ChordContainer } from "../chordcontainer/chordcontainer";
-import { getSingleChord } from "../../../util/extensions/notes/notes";
 
 export function ChordSelector({chords}: TChordMethod) {
     const [uid] = useState(uuid())
 const [notes, setNotes] = useState<string[]>([])
 return (<><h2>Chord</h2><span className={dStyles.bold}>Select a Chord</span>
 <Piano higher={true} highlight={[]} cb={(e: React.MouseEvent<SVGElement>): void => {
-    let el = e.target as SVGElement
-    el.classList.toggle(kStyles.show)
+    let path = e.target as SVGElement
+    path.classList.toggle(kStyles.show)
     let selected: string[] = []
-    el.parentNode?.parentNode?.querySelectorAll(`.${kStyles.show}`).forEach((e) => {
-        let el = e as SVGElement
-        let note = el.dataset.natural ? el.dataset.natural : el.dataset.sharp
-        if (note?.length) {
-            selected.push(note)
+    let g = path.parentNode
+    if (g) {
+        let key = g.parentNode
+        if (key) {
+            key.querySelectorAll(`.${kStyles.show}`).forEach((e) => {
+                let el = e as SVGElement
+                let note: string | undefined = undefined
+                if (el.dataset.natural) {
+                    note = el.dataset.natural
+                } else {
+                    note = el.dataset.sharp
+                }
+                if (note) {
+                    if (note.length) {
+                        selected.push(note)
+                    }
+                }
+            })
+            setNotes(() => selected )
+            chords((prev) => {
+                prev[uid] = selected
+                return {...prev}
+            })
+            let keyContainer = key.parentNode
+            if (keyContainer) {
+                let card = keyContainer.parentNode
+                if (card) {
+                    let generated = card.querySelector(`.generated`)
+                    if (generated) {
+                        generated.remove()
+                    }
+                }
+            }
         }
-    })
-    setNotes(() => selected )
-    chords((prev) => {
-        prev[uid] = selected
-        return {...prev}
-    })
-    el.parentNode?.parentNode?.parentNode?.parentNode?.querySelector(`.generated`)?.remove()
+    }
 } } />
 <ChordContainer notes={notes} />
 <DeleteParent cb={() => {
