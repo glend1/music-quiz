d942d74d9c07f10fa69bce2894ad923f9a0320b6
psyco <psycotik666@gmail.com>
2022-07-05 11:56:40 +0100

    easily increased some code coverage

--- code/components/compound/questioncontrols/questioncontrols.tsx
+++ code/components/compound/questioncontrols/questioncontrols.tsx
@@ -13,9 +13,9 @@ export function QuestionControls({ type, chord, root, interval, question, curren
     return (
         <>
             <h3>{type}</h3>
-            { root && root.note ? <h3>{root?.note}</h3> : ""}
-            { type == "Chord" ? <h3>{chord?.symbol}</h3> : ""}
-            { type == "Interval" ? <h3>{interval?.description}</h3> : ""}
+            { root && root.note ? <h3>{root.note}</h3> : ""}
+            { type == "Chord" && chord ? <h3>{chord.symbol}</h3> : ""}
+            { type == "Interval" && interval ? <h3>{interval.description}</h3> : ""}
             <CheckBox id={'viewQuestions'} label={'Show Answer'} bool={showAnswer} toggle={toggleViewQuestions}/>
             { question && showAnswer ? <h3 className={styles.answer}>{
                 question.map((e, i) => {
