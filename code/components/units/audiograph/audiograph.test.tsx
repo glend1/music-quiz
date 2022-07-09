54d156d24d4ee67453e2cf53b98c464fa9028db4
psyco <psycotik666@gmail.com>
2022-07-09 11:51:38 +0100

    added basic testing for audiograph

--- code/components/units/audiograph/audiograph.test.tsx
+++ code/components/units/audiograph/audiograph.test.tsx
@@ -3,21 +3,21 @@ import { AudioGraph } from "./audiograph"
 import { queries } from "@testing-library/dom/types/"
 
 describe("audiograph", () => {
-    // beforeAll(() => {
-    //     class ResizeObserver {
-    //         observe() {}
-    //         disconnect() {}
-    //     }
-    //     window.ResizeObserver = ResizeObserver as any;
-    // })
-    // it("Should render a canvas", async () => {
-    //     let ag: HTMLElement
-    //     await waitFor(() => {
-    //         ag = render(<AudioGraph freq={500}/>).container
-    //     }).then(() => {
-    //         expect(ag).toBeVisible()
-    //     })
-    // })
+    beforeAll(() => {
+        class ResizeObserver {
+            observe() {}
+            disconnect() {}
+        }
+        window.ResizeObserver = ResizeObserver as any;
+    })
+    it("Should render a canvas", async () => {
+        let ag: HTMLElement
+        await waitFor(() => {
+            ag = render(<AudioGraph freq={null}/>).container
+        }).then(() => {
+            expect(ag).toBeVisible()
+        })
+    })
 //     it("Should fill up the graph with data", async () => {
 //         let ag: RenderResult<typeof queries, HTMLElement>
 //         await waitFor(() => {
