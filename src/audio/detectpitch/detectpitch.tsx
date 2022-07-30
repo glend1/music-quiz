9fcf3f117cd049667cdf1b9c77bb9666890aca88
psyco <psycotik666@gmail.com>
2022-07-30 11:55:20 +0100

    i accidently removed pitch detection while trying to debug. i have replaced it

--- src/audio/detectpitch/detectpitch.tsx
+++ src/audio/detectpitch/detectpitch.tsx
@@ -13,7 +13,36 @@ export function DetectPitch({ cb }: IPitch) {
 	const { context: audioContext } = useAudioContext();
 	const { bool: listening, toggle: toggleListening } = useBoolean(false);
 	const { set: setPitch, clear: clearPitch } = useInterval();
-	useEffect(() => {}, [listening]);
+	useEffect(() => {
+		if (listening) {
+			if (audioContext?.state === "running") {
+				const analyserNode = audioContext.createAnalyser();
+				navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+					let sourceNode = audioContext.createMediaStreamSource(stream);
+					sourceNode.connect(analyserNode);
+					const detector = PitchDetector.forFloat32Array(analyserNode.fftSize);
+					const input = new Float32Array(detector.inputLength);
+					setPitch(() => {
+						analyserNode.getFloatTimeDomainData(input);
+						const [pitch, clarity] = detector.findPitch(
+							input,
+							audioContext.sampleRate
+						);
+						if (pitch >= 32.11) {
+							cb({
+								pitch: Math.round(pitch * 1000) / 1000,
+								clarity: Math.round(clarity * 100),
+							});
+						} else {
+							cb({ pitch: null, clarity: 0 });
+						}
+					}, 100);
+				});
+			}
+		} else {
+			clearPitch();
+		}
+	}, [listening]);
 	return audioContext ? (
 		<button onClick={toggleListening}>{listening ? "Mute" : "Unmute"}</button>
 	) : (
