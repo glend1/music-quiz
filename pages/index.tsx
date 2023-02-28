8aab8c2b178955ab23da9fd85a22520cb0b0f027
glen <glen_dovey@hotmail.com>
2023-02-28 16:21:47 +0000

    added a feature set to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 export default function Index() {
 	return (
-		<section>
-			<h1>Home</h1>
-		</section>
+		<main>
+			<p>Available questions should be;</p>
+			<ul>
+				<li>Note</li>
+				<li>Chord</li>
+				<li>Interval</li>
+				<li>Scale</li>
+			</ul>
+			<p>
+				Each question should allow you to see the question, optionally show the
+				stave or play audio.
+			</p>
+			<p>Each question should be answerable with;</p>
+			<ul>
+				<li>Audio</li>
+				<li>Midi</li>
+				<li>Mouse</li>
+				<li>Keyboard</li>
+			</ul>
+			<p>Each question should show the results.</p>
+			<p>Each test should timed or a chosen number of questions</p>
+		</main>
 	);
 }
