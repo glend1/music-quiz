8b7969c0835f180f90fd46430e588fb8930b4095
glen <glen_dovey@hotmail.com>
2023-03-29 19:43:13 +0100

    docs: add note about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,35 @@
 export default function Index() {
 	return (
 		<main>
-			<p>Available questions should be;</p>
-			<ul>
-				<li>Note</li>
-				<li>Chord</li>
-				<li>Interval</li>
-				<li>Scale</li>
-			</ul>
-			<p>
-				Each question should allow you to see the question, optionally show the
-				stave or play audio.
-			</p>
-			<p>Each question should be answerable with;</p>
-			<ul>
-				<li>Audio</li>
-				<li>Midi</li>
-				<li>Mouse</li>
-				<li>Keyboard</li>
-			</ul>
-			<p>Each question should show the results.</p>
-			<p>Each test should timed or a chosen number of questions</p>
+			<section>
+				<p>Available questions should be;</p>
+				<ul>
+					<li>Note</li>
+					<li>Chord</li>
+					<li>Interval</li>
+					<li>Scale</li>
+				</ul>
+				<p>
+					Each question should allow you to see the question, optionally show
+					the stave or play audio.
+				</p>
+				<p>Each question should be answerable with;</p>
+				<ul>
+					<li>Audio</li>
+					<li>Midi</li>
+					<li>Mouse</li>
+					<li>Keyboard</li>
+				</ul>
+				<p>Each question should show the results.</p>
+				<p>Each test should timed or a chosen number of questions</p>
+			</section>
+			<section>
+				<p>Implement ML</p>
+				<ul>
+					<li>Store Q&amp;A&apos;s in IndexedDB (dexie.js)</li>
+					<li>Use machine learning (tensorflow) to generate questions</li>
+				</ul>
+			</section>
 		</main>
 	);
 }
